eff05a04f89f50d1798a7449f1f94d5302027c59
KITSTuhinChandra <tuhin.chandra@kingfisher.com>
2018-12-27 15:17:25 +0000

    Initial learning

---
+++ ProductAPI/index.js
@@ -0,0 +1,33 @@
+var http = require('http');
+var express=require('express');
+
+var expressApp=express();
+var serverInstance1=http.createServer(expressApp);
+
+var products=[
+    {"ean":1, "displayName":"Product A"},
+    {"ean":2, "displayName":"Product B"},
+    {"ean":3, "displayName":"Product C"}
+
+];
+
+expressApp.get("/getAllProducts",function(request, response){
+    response.json(products);
+})
+
+expressApp.get("/getProductByEAN",function(request, response){
+    var ean=request.query.ean;
+
+    var matchedProduct = products.filter(function(item) {
+        return item.ean == ean;
+      });
+    response.json(matchedProduct);
+})
+
+serverInstance1.listen(8181,function(error, success){
+    if(error){
+        console.log("Error starting server"+error);
+    }else{
+        console.log("Servert started"+success);
+    }
+})
